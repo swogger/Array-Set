42f834160abb3283925ea55e8346b059652abc53
swogger <dimitar@swogo.com>
2014-08-09 01:13:42 +0100

    reduce code

--- ArraySet.js
+++ ArraySet.js
@@ -1,42 +1,32 @@
 /**
 Created by Dimitar Kazakov dimitar.kazakov@gmail.com on 03/08/14. ( GPL 2014 )
 */
-
 var union = function (a, b) {
     "use strict";
     a = a.concat(b);
     return a.filter(function (e, p) {
-        if (a.indexOf(e) === p) {
-            return true;
-        }
+        return (a.indexOf(e) === p);
     });
 };
 
 var difference = function (a, b) {
     "use strict";
     return a.filter(function (e) {
-        if (b.indexOf(e) === -1) {
-            return true;
-        }
+        return (b.indexOf(e) === -1);
     });
 }
 
 var intersection = function (a, b) {
     "use strict";
     return a.filter(function (e) {
-        if (b.indexOf(e) !== -1) {
-            return true;
-        }
+        return (b.indexOf(e) !== -1);
     });
 };
 
 var exclusion = function (a, b) {
     "use strict";
-    a= a.concat(b);
+    a = a.concat(b);
     return a.filter(function (e) {
-        if (a.indexOf(e) === a.lastIndexOf(e)) {
-            return true;
-        }
+        return (a.indexOf(e) === a.lastIndexOf(e)) ;
     });
 };
-
